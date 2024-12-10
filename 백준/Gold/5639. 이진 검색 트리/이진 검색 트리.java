840a5d37e7b092bb3d058b51229f4dc103c749b4
SeongHo-Moon <50991486+now-start@users.noreply.github.com>
2024-12-10 23:41:33 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 624 ms, Memory: 22192 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,136 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.SortedSet;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/*
+    BAEKJOON 5639 이진 검색 트리
+    https://www.acmicpc.net/problem/5639
+*/
+
+public class Main {
+
+    static FastIO io;
+
+    public static void main(String[] args) throws Exception {
+        io = new FastIO();
+        // start code
+        Node node = null;
+        try {
+            while (true) {
+                node = insert(node, io.nextInt());
+            }
+        } catch (Exception e) {
+            // nothing
+        }
+
+        postOrder(node);
+
+        io.print();
+    }
+
+    static Node insert(Node root, int value) {
+        if (root == null) {
+            return new Node(value);
+        }
+        if (value < root.value) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);  // 왼쪽 서브트리
+        postOrder(root.right); // 오른쪽 서브트리
+        io.appendln(root.value);
+    }
+
+    static class Node {
+        int value;
+        Node left, right;
+
+        Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class FastIO {
+
+        BufferedReader br;
+        BufferedWriter bw;
+        StringTokenizer st;
+        StringBuilder sb;
+
+        public FastIO() {
+            br = new BufferedReader(new InputStreamReader(in));
+            bw = new BufferedWriter(new OutputStreamWriter(out));
+            sb = new StringBuilder();
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        FastIO append(Object str) {
+            sb.append(str);
+            return this;
+        }
+
+        FastIO appendln(Object str) {
+            sb.append(str).append("\n");
+            return this;
+        }
+
+        void print() throws IOException {
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void print(Object str) throws IOException {
+            sb.append(str);
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void close() throws IOException {
+            br.close();
+            bw.close();
+        }
+    }
+}
