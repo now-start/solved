b063cd69125c520f40da08f37741de898955c92e
SeongHo-Moon <50991486+now-start@users.noreply.github.com>
2024-12-10 21:41:57 +0900

    [Bronze IV] Title: RICE SACK, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/9699. RICE SACK/RICE SACK.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/*
+    BAEKJOON 9699 RICE SACK
+    https://www.acmicpc.net/problem/9699
+*/
+
+public class Main {
+
+    static FastIO io;
+
+    public static void main(String[] args) throws Exception {
+        io = new FastIO();
+        // start code
+        int t = io.nextInt();
+
+        for(int tc = 1; tc <= t; tc++) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < 5; i++) {
+                max = Math.max(max, io.nextInt());
+            }
+            io.append("Case #").append(tc).append(": ").appendln(max);
+        }
+
+        io.print();
+    }
+
+    static class FastIO {
+
+        BufferedReader br;
+        BufferedWriter bw;
+        StringTokenizer st;
+        StringBuilder sb;
+
+        public FastIO() {
+            br = new BufferedReader(new InputStreamReader(in));
+            bw = new BufferedWriter(new OutputStreamWriter(out));
+            sb = new StringBuilder();
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        FastIO append(Object str) {
+            sb.append(str);
+            return this;
+        }
+
+        FastIO appendln(Object str) {
+            sb.append(str).append("\n");
+            return this;
+        }
+
+        void print() throws IOException {
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void print(Object str) throws IOException {
+            sb.append(str);
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void close() throws IOException {
+            br.close();
+            bw.close();
+        }
+    }
+}
