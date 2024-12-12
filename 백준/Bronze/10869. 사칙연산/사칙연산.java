503601b914dfa9332bb3a08c75a4c971c8b0a350
SeongHo-Moon <50991486+now-start@users.noreply.github.com>
2024-12-12 20:47:35 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/*
+    BAEKJOON 10869 사칙연산
+    https://www.acmicpc.net/problem/10869
+*/
+
+public class Main {
+
+    static FastIO io;
+
+    public static void main(String[] args) throws Exception {
+        io = new FastIO();
+        // start code
+        int a = io.nextInt();
+        int b = io.nextInt();
+        
+        io.appendln(a + b)
+            .appendln(a - b)
+            .appendln(a * b)
+            .appendln(a / b)
+            .append(a % b);
+
+        io.print();
+    }
+
+    static class FastIO {
+        BufferedReader br;
+        BufferedWriter bw;
+        StringTokenizer st;
+        StringBuilder sb;
+
+        public FastIO() {
+            br = new BufferedReader(new InputStreamReader(in));
+            bw = new BufferedWriter(new OutputStreamWriter(out));
+            sb = new StringBuilder();
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        FastIO append(Object str) {
+            sb.append(str);
+            return this;
+        }
+
+        FastIO appendln(Object str) {
+            sb.append(str).append("\n");
+            return this;
+        }
+
+        void print() throws IOException {
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void print(Object str) throws IOException {
+            sb.append(str);
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void close() throws IOException {
+            br.close();
+            bw.close();
+        }
+    }
+}
