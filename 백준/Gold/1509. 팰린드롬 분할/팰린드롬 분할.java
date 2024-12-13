223dcab48ea7d1837c5e6f6324875a604ed8dca5
SeongHo-Moon <50991486+now-start@users.noreply.github.com>
2024-12-14 00:26:16 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 192 ms, Memory: 22364 KB -BaekjoonHub

---
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -0,0 +1,122 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/*
+    BAEKJOON 1509 팰린드롬 분할
+    https://www.acmicpc.net/problem/1509
+*/
+
+public class Main {
+
+    static FastIO io;
+
+    public static void main(String[] args) throws Exception {
+        io = new FastIO();
+        // start code
+        String s = io.next();
+
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (s.charAt(j) == s.charAt(j + i) && dp[j + 1][j + i - 1]) {
+                    dp[j][j + i] = true;
+                }
+            }
+        }
+
+        int[] d = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[i] = Integer.MAX_VALUE;
+            for (int j = 0; j <= i; j++) {
+                if (dp[j][i]) {
+                    d[i] = Math.min(d[i], j == 0 ? 1 : d[j - 1] + 1);
+                }
+            }
+        }
+
+        io.print(d[n - 1]);
+    }
+
+    static class FastIO {
+        BufferedReader br;
+        BufferedWriter bw;
+        StringTokenizer st;
+        StringBuilder sb;
+
+        public FastIO() {
+            br = new BufferedReader(new InputStreamReader(in));
+            bw = new BufferedWriter(new OutputStreamWriter(out));
+            sb = new StringBuilder();
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        FastIO append(Object str) {
+            sb.append(str);
+            return this;
+        }
+
+        FastIO appendln(Object str) {
+            sb.append(str).append("\n");
+            return this;
+        }
+
+        void print() throws IOException {
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void print(Object str) throws IOException {
+            sb.append(str);
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void close() throws IOException {
+            br.close();
+            bw.close();
+        }
+    }
+}
