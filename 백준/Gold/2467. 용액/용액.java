6bf0f0ce921a7d884fcc01690542183147a8727d
SeongHo-Moon <50991486+now-start@users.noreply.github.com>
2024-12-14 04:36:51 +0900

    [Gold V] Title: 용액, Time: 348 ms, Memory: 30896 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/*
+    BAEKJOON 2467 용액
+    https://www.acmicpc.net/problem/2467
+*/
+
+public class Main {
+
+    static FastIO io;
+
+    public static void main(String[] args) throws Exception {
+        io = new FastIO();
+        // start code
+        int n = io.nextInt();
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(io.nextInt());
+        }
+        Collections.sort(arr);
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        int result = Integer.MAX_VALUE;
+
+        int x=0;
+        int y=0;
+        while (left < right) {
+            int sum = arr.get(left) + arr.get(right);
+            if (Math.abs(sum) < result) {
+                result = Math.abs(sum);
+                x = arr.get(left);
+                y = arr.get(right);
+            }
+
+            if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        io.append(x).append(" ").append(y).append("\n");
+        io.print();
+    }
+
+    static class FastIO {
+        BufferedReader br;
+        BufferedWriter bw;
+        StringTokenizer st;
+        StringBuilder sb;
+
+        public FastIO() {
+            br = new BufferedReader(new InputStreamReader(in));
+            bw = new BufferedWriter(new OutputStreamWriter(out));
+            sb = new StringBuilder();
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        FastIO append(Object str) {
+            sb.append(str);
+            return this;
+        }
+
+        FastIO appendln(Object str) {
+            sb.append(str).append("\n");
+            return this;
+        }
+
+        void print() throws IOException {
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void print(Object str) throws IOException {
+            sb.append(str);
+            bw.write(sb.toString());
+            bw.flush();
+        }
+
+        void close() throws IOException {
+            br.close();
+            bw.close();
+        }
+    }
+}
